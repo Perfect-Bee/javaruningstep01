51fb875c329f1e728bd5a1b7c0d3e6b33a603f12
parva <2021142023@tukorea.ac.kr>
2025-12-10 20:11:28 +0900

    상속 : Champion에서 중복되는거 상속받아서 사용. Champion과는 별개의 생성자를 생성하여 사용

--- src/Ashe.java
+++ src/Ashe.java
@@ -1,64 +1,24 @@
-public class Ashe {
-    // 필드 데이터
-    private String name;        // 에쉬
-    private int level;          // 레밸
-    private int hp;             // 체력
-    private int attackDamage;   // 공격력
-    private int defence;        // 방어력
-
-    public Ashe(String name, int level, int hp, int attackDamage, int defence){
-        this.name = name;
-        this.level = level;
-        this.hp = hp;
-        this.attackDamage = attackDamage;
-        this.defence = defence;
-    }
-
-    // 행위 설정
-    // 피해량
-    public void takeDamage(int damage){
-        // 내 체력 - (내 방어력 - 적의 공격력) : 내 체력 - 실제 데미지
-        int actualDamage = damage - this.defence;
-        if( actualDamage < 0 ) actualDamage = 0;
-
-        this.hp -= actualDamage;
-
-        System.out.println("[ " + this.name + " ]" + "이(가) " + actualDamage + " 피해를 받았습니다!");
-        System.out.println("현재 체력: " + this.hp);
-    }
-
-    // 체력
-    public int getHp(){return this.hp;}
-    // 이름
-    public String getName(){return this.name;}
-    // 이름 변경
-    public void changeName(String name) {
-        String name2 = this.name;
-        this.name = name;
-        System.out.println(name2 + "의 변경된 이름은 : " + this.name);
+// Ashe가 Champion의 것을 상속받음
+public class Ashe extends Champion {
+    // Ashe를 위한 생성자 재설정
+    public Ashe(String name, int level, int hp, int attackDamage, int defence) {
+        // 부모(Champion)의 생성자를 가져온다.
+        super(name, level, hp, attackDamage, defence);
     }
 
     // 타격량 : 에쉬가 가렌을 공격했다.
-    public void basicAttackToGaren(Garen garen){
-        System.out.println("[ " + this.name + " ]" + "이(가) " + "[ " + garen.getName() + " ]" + "을(를) 기본 공격!");
-        garen.takeDamage(this.attackDamage);//가렌의 takeDamage에 에쉬의 attackDamage 입력 = 피해량
+    public void basicAttackToGaren(Garen target){
+        // this는 상속받은거니 super로 바꾸고, name도 public에서 써야하니 getName이다.
+        System.out.println("[ " + getName() + " ]" + "이(가) " + "[ " + target.getName() + " ]" + "을(를) 기본 공격!");
+        target.takeDamage(super.getAttackDamage());
     }
 
-    // 스킬
-    public void skillQAttackToGaren(Garen target){
-        System.out.println("[ " + this.name + " ]" + "이(가) " + "[ " + target.getName() + " ]" + "을(를) 스킬 Q 공격!");
-        target.takeDamage(this.attackDamage * 2);
+    @Override // @은 문법임. 정의.
+    public void useQ(){
+        // 에쉬만의 Q 스킬
     }
-    public void skillWAttackToGaren(Garen target){
-        System.out.println("[ " + this.name + " ]" + "이(가) " + "[ " + target.getName() + " ]" + "을(를) 스킬 W 공격!");
-        target.takeDamage(this.attackDamage * 3);
-    }
-    public void skillEAttackToGaren(Garen target){
-        System.out.println("[ " + this.name + " ]" + "이(가) " + "[ " + target.getName() + " ]" + "을(를) 스킬 E 공격!");
-        target.takeDamage(this.attackDamage * 4);
-    }
-    public void skillRAttackToGaren(Garen target){
-        System.out.println("[ " + this.name + " ]" + "이(가) " + "[ " + target.getName() + " ]" + "을(를) 스킬 R 공격!");
-        target.takeDamage(this.attackDamage * 5);
-    }
+    public void useW(){}
+    public void useE(){}
+    public void useR(){}
+
 }
--- src/Garen.java
+++ src/Garen.java
@@ -1,65 +1,24 @@
-public class Garen {
-    // 필드 데이터
-    private String name;        // 가렌 이름(
-    private int level;          // 레밸
-    private int hp;             // 체력
-    private int attackDamage;   // 공격력
-    private int defence;        // 방어력
-
-    // 생성자
-    public Garen(String name, int level, int hp, int attackDamage, int defence){
-        this.name = name;
-        this.level = level;
-        this.hp = hp;
-        this.attackDamage = attackDamage;
-        this.defence = defence;
+// 상속 : extends
+// 중복 : 필드 데이터, 생성자, 피해량, getter, setter, 타격량, 스킬)
+public class Garen extends Champion {
+    // Garen를 위한 생성자 재설정
+    public Garen(String name, int level, int hp, int attackDamage, int defence) {
+        // 부모(Champion)의 생성자를 가져온다.
+        super(name, level, hp, attackDamage, defence);
     }
 
-    // 행위 설정
-    // 피해량
-    public void takeDamage(int damage){
-        // 내 체력 - (내 방어력 - 적의 공격력) : 내 체력 - 실제 데미지
-        int actualDamage = damage - this.defence;
-        if( actualDamage < 0 ) actualDamage = 0;
-
-        this.hp -= actualDamage;
-
-        System.out.println("[ " + this.name + " ]" + "이(가) " + actualDamage + " 피해를 받았습니다!");
-        System.out.println("현재 체력: " + this.hp);
-    }
-
-    // 체력
-    public int getHp(){return this.hp;}
-    // 이름
-    public String getName(){return this.name;}
-    // 이름 변경
-    public void changeName(String name){
-        String name2 = this.name;
-        this.name = name;
-        System.out.println(name2 + "의 변경된 이름은 : " + this.name);
-    }
 
     // 타격량 : 가렌이 에쉬를 공격했다.
-    public void basicAttackToAshe(Ashe ashe){
-        System.out.println("[ " + this.name + " ]" + "이(가) " + "[ " + ashe.getName() + " ]" + "을(를) 기본 공격!");
-        ashe.takeDamage(this.attackDamage);//에쉬의 takeDamage에 가렌의 attackDamage 입력 = 피해량
+    public void basicAttackToAshe(Ashe target){
+        System.out.println("[ " + super.getName() + " ]" + "이(가) " + "[ " + target.getName() + " ]" + "을(를) 기본 공격!");
+        target.takeDamage(super.getAttackDamage());
     }
 
-    // 스킬
-    public void skillQAttackToAshe(Ashe target){
-        System.out.println("[ " + this.name + " ]" + "이(가) " + "[ " + target.getName() + " ]" + "을(를) 스킬 Q 공격!");
-        target.takeDamage(this.attackDamage * 2);
-    }
-    public void skillWAttackToAshe(Ashe target){
-        System.out.println("[ " + this.name + " ]" + "이(가) " + "[ " + target.getName() + " ]" + "을(를) 스킬 W 공격!");
-        target.takeDamage(this.attackDamage * 3);
-    }
-    public void skillEAttackToAshe(Ashe target){
-        System.out.println("[ " + this.name + " ]" + "이(가) " + "[ " + target.getName() + " ]" + "을(를) 스킬 E 공격!");
-        target.takeDamage(this.attackDamage * 4);
-    }
-    public void skillRAttackToAshe(Ashe target){
-        System.out.println("[ " + this.name + " ]" + "이(가) " + "[ " + target.getName() + " ]" + "을(를) 스킬 R 공격!");
-        target.takeDamage(this.attackDamage * 5);
-    }
+    // 스킬 : 내부 로직은 알아서 채우기
+    @Override
+    public void useQ(){}
+    public void useW(){}
+    public void useE(){}
+    public void useR(){}
+
 }
